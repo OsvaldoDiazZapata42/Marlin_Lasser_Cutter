3d959a1a487c2e70ebb2b842fea24523e8ea764d
OsvaldoDiaz <osvaldo.diaz.zapata@gmail.com>
2024-07-25 12:02:27 -0500

    Reduce the aceleration to avoid dots

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1100,6 +1100,10 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
+//TODO OSVALDO
+/*
+This setting defines the maximum speed at which each axis can move. In mm/seg
+ */
 #define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
@@ -1113,7 +1117,10 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 10000 }
+/*
+This setting defines the maximum acceleration for each axis. in mm/s2
+*/
+#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 10000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
